49c9cc7aba02772a5d5d4ba2ad6140a52fae37f1
송우인 <sowintildeblue@gmail.com>
2026-01-18 17:34:43 +0900

    브라우저 콘솔 응답

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def maketag(isopen,tagname):
         else:
             return f"</{tagname}>"
 
-nametag = ["h1","p","div"]
+nametag = ["h1","p","div","script"]
         
 h1_content = "안녕하세요!"
 bodyh = maketag(True,nametag[0]) + h1_content + maketag(False,nametag[0])
@@ -15,17 +15,21 @@ bodyh = maketag(True,nametag[0]) + h1_content + maketag(False,nametag[0])
 p_content = "만나서 반갑습니다."
 bodyp = maketag(True,nametag[1]) + p_content + maketag(False,nametag[1])
 
+log_content = "console.log('응답을 받음!!');"
+logprint = maketag(True,nametag[3]) + log_content + maketag(False,nametag[3])
 
 #server get 응답 설정
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        print(f"[Log] 요청 수신: {self.path}")
         self.send_response(200)
         self.send_header("Content-type", "text/html; charset=utf-8")
         self.end_headers()
 
         self.wfile.write(bodyh.encode('UTF-8'))
         self.wfile.write(bodyp.encode('UTF-8'))
-
+        self.wfile.write(logprint.encode('UTF-8'))
+        print("[Log] 응답 전송 완료")
 
 
     
